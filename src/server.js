37072d76aa3f6baf40063f6e2845a97668c3c02e
Michael Prather <mprather1@gmail.com>
2017-09-11 23:19:53 -0500

    update

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import path from 'path'
+import favicon from 'serve-favicon'
+import http from 'http'
+
+const _pkg = require(path.join(path.dirname(__dirname), 'package.json'))
+const _bootstrapDir = require.resolve('bootstrap').match(/.*\/node_modules\/[^/]+\//)[0]
+
+var app = express()
+var server = http.Server(app)
+var port = process.env['PORT']
+
+app.use(favicon(path.join(__dirname, 'resources', 'images', 'favicon.png')))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use('/css', express.static(path.join(_bootstrapDir, 'dist', 'css')))
+app.use(express.static(path.join(__dirname, 'static')))
+
+server.on('request', (req, res) => {
+  console.log(req.method, req.url)
+})
+
+server.on('listening', () => {
+  console.log(`${_pkg.name} version ${_pkg.version} is listening on port ${port}...`)
+})
+
+server.listen(port)
